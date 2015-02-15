2568dc89080d5a81c5fd4d555909ab78340a27ae
erkkiperkele <aymeric.grail@gmail.com>
2015-02-15 13:28:29 -0500

    Learning: sending a message to a new activity and displaying it to the user.

---
+++ Coachy/app/src/main/java/com/erkkiperkele/coachy/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+package com.erkkiperkele.coachy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the textView
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the textView as the activity layout
+        setContentView(textView);
+    }
+}
