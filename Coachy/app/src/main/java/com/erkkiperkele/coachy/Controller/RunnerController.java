278d89578c39b41dcb73daeb109075114383ffb8
erkkiperkele <aymeric.grail@gmail.com>
2015-04-25 12:31:13 -0400

    Added shell for the RunnerController

---
+++ Coachy/app/src/main/java/com/erkkiperkele/coachy/Controller/RunnerController.java
@@ -0,0 +1,52 @@
+package com.erkkiperkele.coachy.Controller;
+
+
+import com.erkkiperkele.coachy.Model.RunnerModel;
+
+public class RunnerController {
+    private RunnerModel _runnerModel;
+
+    public RunnerController(RunnerModel _runnerModel) {
+        this._runnerModel = _runnerModel;
+    }
+
+    public void getRunnerWorkoutStats() {
+        throw new NoSuchMethodError();
+    }
+
+    public void getRunnerAllTimeStats() {
+        throw new NoSuchMethodError();
+    }
+
+    public void getTeam() {
+        throw new NoSuchMethodError();
+    }
+
+    public void createTeam() {
+        throw new NoSuchMethodError();
+    }
+
+    public void addTeamMember() {
+        throw new NoSuchMethodError();
+    }
+
+    public void removeTeamMember() {
+        throw new NoSuchMethodError();
+    }
+
+    public void getCoachProfile() {
+        throw new NoSuchMethodError();
+    }
+
+    public void createCoachProfile() {
+        throw new NoSuchMethodError();
+    }
+
+    public void updateCoachProfile() {
+        throw new NoSuchMethodError();
+    }
+
+    public void deleteCoachProfile() {
+        throw new NoSuchMethodError();
+    }
+}
